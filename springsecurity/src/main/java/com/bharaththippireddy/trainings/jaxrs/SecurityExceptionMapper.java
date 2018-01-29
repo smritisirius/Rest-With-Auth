5d90e9350083292d0d406068df0f716dffb4efa5
smritisirius <smriti.singh1202@outlook.com>
2018-01-29 23:59:05 +0530

    Adding new project

---
+++ springsecurity/src/main/java/com/bharaththippireddy/trainings/jaxrs/SecurityExceptionMapper.java
@@ -0,0 +1,15 @@
+package com.bharaththippireddy.trainings.jaxrs;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.springframework.security.access.AccessDeniedException;
+
+public class SecurityExceptionMapper implements ExceptionMapper<AccessDeniedException> {
+
+	@Override
+	public Response toResponse(AccessDeniedException arg0) {
+		return Response.status(Response.Status.FORBIDDEN).build();
+	}
+
+}
